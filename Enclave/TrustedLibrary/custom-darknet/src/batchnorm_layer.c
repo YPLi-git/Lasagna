99308a64755519e586c125b521c2a0815028a0c3
ypli-git <liyuepengemail@163.com>
2021-08-16 09:33:27 +0800

    Update batchnorm_layer.c

--- Enclave/TrustedLibrary/custom-darknet/src/batchnorm_layer.c
+++ Enclave/TrustedLibrary/custom-darknet/src/batchnorm_layer.c
@@ -63,7 +63,7 @@ void mean_delta_cpu(float *delta, float *variance, int batch, int filters, int s
         }
         mean_delta[i] *= (-1./sqrt(variance[i] + .00001f));
     }
-}
+} 
 void  variance_delta_cpu(float *x, float *delta, float *mean, float *variance, int batch, int filters, int spatial, float *variance_delta)
 {
  
